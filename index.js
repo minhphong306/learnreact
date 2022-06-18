f5bf3ed873ce875d74e0bd2cb14f8642b13917c8
Do Minh Phong <dominhphong306@gmail.com>
2022-06-18 15:35:55 +0700

    feat: add challenge fun fact

--- index.js
+++ index.js
@@ -26,7 +26,22 @@ const navBar = (
 //     <MainContent/>
 // </div>, document.getElementById('root'))
 
+// Challenge: Build funfact about react page
+const funcFact = (
+    <div>
+        <img src='assets/images/icon.png' width={40}/>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
 ReactDOM.render(
-    navBar,
+    funcFact,
     document.getElementById('root')
 )
